e28dbc0925c967ead859f6480468077469f93e07
Vishwajeet Pawar <vishwajeet.pawarrr@gmail.com>
2024-03-07 08:53:49 +0530

    added api call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,37 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import Starred from './Starred';
+import { useState } from 'react';
 
 const Home = () => {
+  const [searchStr, setSearchStr] = useState('');
+
+  const onSetSearchInputChange = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async (ev) => {
+    ev.preventDefault(); //preventDeafault will help us to stop redirecting
+
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`);
+    const body = await response.json();
+    console.log(body)    
+      
+  };
+
   return (
     <div>
-        <h3>Home Page</h3>
-        {/* <Link to="/Starred">Go to starred Page</Link> */}
-    </div>);
+      <form onSubmit={onSearch}>
+        <input
+          type="text"
+          value={searchStr}
+          onChange={onSetSearchInputChange}
+        />
+        {/* value is used to make two way data binding */}
+
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
