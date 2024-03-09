02ec0be0ccfea2d3e20b676da77a0897820f59c1
Vishwajeet Pawar <vishwajeet.pawarrr@gmail.com>
2024-03-09 13:03:29 +0530

    made read more dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
+import Show from './pages/Show'
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/Starred" element={<Starred />} />
         </Route>
+
+        <Route path='/show/:showId' element={<Show />}/>
+
         <Route path="*" element={<div>PAGE NOT FOUND</div>} />
 
         {/* <Route path="/" element={<App />}>
