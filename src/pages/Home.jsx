e91e74ae18c120fee77c3b91a255ac3790ba3693
Vishwajeet Pawar <vishwajeet.pawarrr@gmail.com>
2024-03-12 19:15:36 +0530

    added useQuery library function in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,21 @@ import { searchForPeople, searchForShows } from '../api/tvmaze';
 import SearchForm from '../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
+import { useQuery } from '@tanstack/react-query';
 
 const Home = () => {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+
+  const [filter, setFilter] = useState(null);
+
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () => filter.searchOption === 'shows' ? searchForShows(filter.q) : searchForPeople(filter.q),
+    enabled: !!filter,
+    refetchOnWindowFocus: false
+  });
+
+  // const [apiData, setApiData] = useState(null);
+  // const [apiDataError, setApiDataError] = useState(null);
 
   const onSearch = async ({ q, searchOption }) => {
     // ev.preventDefault(); //preventDeafault will help us to stop redirecting
@@ -19,20 +30,26 @@ const Home = () => {
 
     // const body = awaut apiGet(`/search/shows?q=${searchStr}`)
 
-    try {
-      setApiDataError(null);
+    // ---
 
-      let result;
+    // try {
+    //   setApiDataError(null);
 
-      if (searchOption == 'shows') {
-        result = await searchForShows(q);
-      } else {
-        result = await searchForPeople(q);
-      }
-      setApiData(result);
-    } catch (error) {
-      setApiDataError(error);
-    }
+    //   let result;
+
+    //   if (searchOption == 'shows') {
+    //     result = await searchForShows(q);
+    //   } else {
+    //     result = await searchForPeople(q);
+    //   }
+    //   setApiData(result);
+    // } catch (error) {
+    //   setApiDataError(error);
+    // }
+
+    // ----
+
+    setFilter({q, searchOption});
   };
 
   const renderApiData = () => {
@@ -40,8 +57,8 @@ const Home = () => {
       return <div>ERROR OCCURED: {apiDataError.message}</div>;
     }
 
-    if (apiData?.length === 0){
-      <div> Data Not Found </div>
+    if (apiData?.length === 0) {
+      <div> Data Not Found </div>;
     }
 
     if (apiData && apiData.length > 0) {
